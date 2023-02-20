b650d1297b283ebb48715325ca87ae018b074b6b
Kalel Poeta <kalel.poeta@orbisag.ch>
2023-02-20 15:20:29 +0100

    Added api key and secret from env file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
-
 import cors from "cors";
+import { config } from "dotenv";
 
+config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+const api_key = process.env.API_KEY;
+const api_secret = process.env.API_SECRET;
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
